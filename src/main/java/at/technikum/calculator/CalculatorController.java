c3b6a84bbd88d0b817b4d0bd7edf408451fbfbe8
Matyus Wolfgang <wi21b055@technikum-wien.at>
2022-06-15 20:27:46 +0200

    Add feature

--- src/main/java/at/technikum/calculator/CalculatorController.java
+++ src/main/java/at/technikum/calculator/CalculatorController.java
@@ -8,5 +8,5 @@ public class CalculatorController {
     @GetMapping("/sum")
     public int sum(@RequestParam int a, @RequestParam int b) {
         return a + b;
-    }
+    } //test: "http://localhost:8080/sum?a=3&b=4" in browser
 }
---
+++ src/test/java/at/technikum/calculator/CalculatorTest.java
@@ -0,0 +1,44 @@
+package at.technikum.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    private Calculator calc;
+
+    @BeforeEach
+    void initialise(){
+        calc = new Calculator();
+    }
+
+    @Test
+    void sum(){
+        //arrange
+        Calculator calc = new Calculator();
+        int a = 5;
+        int b = 10;
+
+        // act
+        int result = calc.sum(a,b);
+
+        //assert
+        assertEquals(15, result);
+
+    }
+
+    @Test
+    void multiplicationTestNormalUsage(){
+        // arrange
+        int a = 2;
+        int b = 3;
+
+        // act
+        int result = calc.multiply(a, b);
+
+        // assert
+        assertEquals(0, result);
+    }
+}
